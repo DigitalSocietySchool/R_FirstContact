c1270062412daf264ee05de3a223871265638713
EMMA-BA <ebeauxis@gmail.com>
2018-11-14 12:19:27 +0100

    More exercises

---
+++ Code/2a_More_Manip_Data.R
@@ -0,0 +1,78 @@
+# data_cbs <- read.csv('../Data/CBS_Growth_Rates.csv')
+
+###########################
+### SELECT WITH PATTERN
+###########################
+# https://github.com/rstudio/cheatsheets/blob/master/data-import.pdf
+
+data_cbs %>%
+  select(starts_with('L'))
+
+data_cbs %>%
+  select(ends_with('s'))
+
+########### EXERCISE #####
+# 
+# Select the columns that contain the growth rates.
+#
+# SOLUTION:
+# data_cbs %>%
+#   select(ends_with('Growth'))
+# 
+###########################
+
+###########################
+### NARROW & WIDE DATA
+###########################
+
+############ Narrow Data ############ 
+# Transform Multiple Columns -> Label+Value Columns
+
+# Transform Multiple Columns -> Label+Value Columns
+data_cbs %>%
+  select(Label, Periods, Output) %>%
+  gather(Output, 
+         key='Output', value='MEuro')
+
+########### EXERCISE #####
+# 
+# Make narrow data for all the growth rate indicators, 
+# and include the Label and Periods information.
+#
+# SOLUTION:
+# data_cbs %>%
+#   filter(Periods > 2001) %>%
+#   select(Label, Periods, ends_with('Growth')) %>%
+#   gather(Labour_Growth, Output_Growth, ValueAdded_Growth, 
+#          key='Indicator', value='Rate')
+# 
+###########################
+
+
+
+############ Wide Data ############ 
+# Transform Label+Value Columns -> Multiple Columns
+data_narrow <- data_cbs %>%
+  select(Label, Periods, Output) %>%
+  gather(Output, 
+         key='Output', value='MEuro')
+
+data_narrow %>% spread(Output, MEuro)
+
+########### EXERCISE #####
+# 
+# Given the narrow data for growth rate (resulting for the previous exercise),
+# make it wide again.
+#
+# SOLUTION:
+# data_narrow <- data_cbs %>%
+#   filter(Periods > 2001) %>%
+#   select(Label,Periods, ends_with('Growth')) %>%
+#   gather(Labour_Growth, Output_Growth, ValueAdded_Growth, 
+#          key='Indicator', value='Rate') 
+# 
+# data_narrow %>% spread(Indicator, Rate)
+# 
+###########################
+
+
--- Code/2b_Viz_Data.R
+++ Code/2b_Viz_Data.R
@@ -1,51 +1,41 @@
-# data_backup <- data_cbs
-# data_cbs <- data_backup
+# data_cbs <- read.csv('../Data/CBS_Growth_Rates.csv') %>% tibble
 
 ###########################
-### MULTIPLE LINES
+### PLOT MULTIPLE LINES
 ###########################
-# https://github.com/rstudio/cheatsheets/blob/master/data-import.pdf
 
-### Narrow Data
-# Transform Multiple Columns -> Label+Value Columns
+############ Visualize using Narrow Data ############ 
 data_cbs %>%
+  # Exclude years without growth rate data
   filter(Periods > 2001) %>%
-  select(Label,Periods, ends_with('Growth')) %>%
-  gather(Labour_Growth, Output_Growth, ValueAdded_Growth, 
-         key='Indicator', value='Rate') 
-
-### Wide Data
-# Transform Label+Value Columns -> Multiple Columns
-data_narrow <- data_cbs %>%
-  filter(Periods > 2001) %>%
-  select(Label,Periods, ends_with('Growth')) %>%
-  gather(Labour_Growth, Output_Growth, ValueAdded_Growth, 
-         key='Indicator', value='Rate') 
-
-data_narrow %>% spread(Indicator, Rate)
-
-
-# Visualize
-data_cbs %>%
-  filter(Periods > 2001) %>%
+  # Filter growth rate data
   select(Label, Periods, ends_with('Growth')) %>%
+  # Make narrow data
   gather(Labour_Growth, Output_Growth, ValueAdded_Growth, 
          key='Indicator', value='Rate') %>%
+  # Now we can plot multiple lines
   ggplot(aes(Periods, Rate, shape=Indicator, col=Indicator)) + 
-  facet_wrap( ~ Label, ncol=3) + # scales='free'
+  facet_wrap( ~ Label, ncol=3) + # Option: scales='free'
   geom_point(alpha=0.5) + geom_line(alpha=0.35)
 
-data_cbs %>%
-  filter(Periods > 2001) %>%
-  mutate(Labour = Labour*1000) %>%
-  gather(Labour, Output, Value.Added, 
-         key='Indicator', value='Rate') %>%
-  ggplot(aes(Periods, Rate, shape=Indicator, col=Indicator)) + 
-  facet_wrap( ~ Label, ncol=3, scales='free') +
-  geom_point(alpha=0.5) + geom_line(alpha=0.35) +
-  ggtitle('Unit of Labour: Year of Employement - Output & Added Value: Million Euros')
 
-
+########### EXERCISE #####
+# 
+# Visualize the raw data (not growth rate) for columns Labour, Output, ValueAdded
+# using multiple lines.
+#
+# SOLUTION:
+# data_cbs %>%
+#   filter(Periods > 2001) %>%
+#   mutate(Labour = Labour*1000) %>%
+#   gather(Labour, Output, ValueAdded, 
+#          key='Indicator', value='Rate') %>%
+#   ggplot(aes(Periods, Rate, shape=Indicator, col=Indicator)) + 
+#   facet_wrap( ~ Label, ncol=3, scales='free') +
+#   geom_point(alpha=0.5) + geom_line(alpha=0.35) +
+#   ggtitle('Unit of Labour: Year of Employement - Output & Added Value: Million Euros')
+# 
+###########################
 
 
 
@@ -60,38 +50,68 @@ data_cbs %>%
 data_cbs %>% 
   filter(Periods > 2001) %>%
   ggplot(aes(Output_Growth, ValueAdded_Growth)) + 
-  # facet_wrap( ~ Label, ncol=3) +
   geom_point(alpha=0.7) + 
   geom_abline(intercept = 0, slope = 1, color="grey", 
               linetype="dashed", size=0.5)
 
-
-data_cbs %>% 
-  filter(Periods > 2001) %>%
-  ggplot(aes(Output_Growth, ValueAdded_Growth, col=Label)) + 
-  facet_wrap( ~ Label, ncol=3, scales='free') +
-  geom_point(alpha=0.7) + geom_line(alpha=0.35) +
-  geom_abline(intercept = 0, slope = 1, color="grey", 
-              linetype="dashed", size=0.5)
+########### EXERCISE #####
+# 
+# 1. Make a visualization for comparing Output Vs Valued Added
+# for each economic activity separately.
+#
+# SOLUTION:
+# data_cbs %>% 
+#   filter(Periods > 2001) %>%
+#   ggplot(aes(Output_Growth, ValueAdded_Growth, col=Label)) + 
+#   facet_wrap( ~ Label, ncol=3, scales='free') 
+#   geom_point(alpha=0.7) + geom_line(alpha=0.35) +
+#   geom_abline(intercept = 0, slope = 1, color="grey", 
+#               linetype="dashed", size=0.5)
+#
+#
+# 2. Make a visualization for comparing the growth rates of Labour and ValueAdded.
+#
+# data_cbs %>%
+#   filter(Periods > 2001) %>%
+#   ggplot(aes(Labour_Growth, ValueAdded_Growth, col=Label)) +
+#   facet_wrap( ~ Label, ncol=3, scales='free') 
+#   geom_point(alpha=0.7) + geom_line(alpha=0.35) +
+#   geom_abline(intercept = 0, slope = 1, color="grey",
+#               linetype="dashed", size=0.5)
+#
+#
+# 3. Make a visualization for comparing the growth rates of Labour and Output.
+#
+# data_cbs %>%
+#   filter(Periods > 2001) %>%
+#   ggplot(aes(Labour_Growth, Output_Growth, col=Label)) +
+#   facet_wrap( ~ Label, ncol=3, scales='free') 
+#   geom_point(alpha=0.7) + geom_line(alpha=0.35) +
+#   geom_abline(intercept = 0, slope = 1, color="grey",
+#               linetype="dashed", size=0.5)
+# 
+# Bonus: Try the visualizatin with and without the option scales='free'
+#
+###########################
 
 ### Raw Numbers
 data_cbs %>% 
   # filter(Periods > 2001) %>%
   # filter(!grepl('^Total', Label)) %>%
-  ggplot(aes(Output, Value.Added, col=Label)) + 
+  ggplot(aes(Output, ValueAdded, col=Label)) + 
   facet_wrap( ~ Label, ncol=3, scales='free') +
   geom_point(alpha=0.7) + geom_line(alpha=0.35) 
 
 
 data_cbs %>% 
-  ggplot(aes(Output, Value.Added, col=Label)) + 
+  ggplot(aes(Output, ValueAdded, col=Label)) + 
   facet_wrap( ~ Label, ncol=3, scales='free') +
   geom_point(alpha=0.7) + geom_line(alpha=0.35) 
   
 
 ### + Time Dimension
 data_cbs %>% 
-  ggplot(aes(Output, Value.Added, col=Periods)) + 
+  ggplot(aes(Output, ValueAdded, col=Periods)) + 
   facet_wrap( ~ Label, ncol=3, scales='free') +
   geom_point(alpha=0.7) +
   geom_path(alpha=0.35)
@@ -116,11 +136,11 @@ data_cbs %>%
 
 data_cbs %>% 
   filter(!grepl('^Total', Label)) %>%
-  ggplot(aes(Periods, Output-Value.Added, col=Label)) + 
+  ggplot(aes(Periods, Output-ValueAdded, col=Label)) + 
   geom_point(alpha=0.35) + geom_line(alpha=0.35, lty=2) +
   stat_smooth(method="lm", size=0.4, se = FALSE)
 
-# Try: Output/Value.Added
+# Try: Output/ValueAdded
 
 ###########################
 # Labour Vs Valued Added
